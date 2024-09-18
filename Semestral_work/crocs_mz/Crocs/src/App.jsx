da5eac3e99c1d5fcfa8495528df9b73ceaf7f9c9
Stelio-IN <stelioinvolution@gmail.com>
2024-09-18 22:29:42 +0200

    Testes bsicos

--- Semestral_work/crocs_mz/Crocs/src/App.jsx
+++ Semestral_work/crocs_mz/Crocs/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './app.css'; 
+import Footer from './component/Footer'; 
+import Nav from './component/Nav'; 
+import Home from './component/Home'; // Componente para a página inicial
+import Login from './component/LoginRegister'; // Componente para a página de login
+import SobreNos from './component/SobreNos'; // Componente para Sobre Nós
+import Loja from './component/Loja'; // Componente para Loja
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App" style={{
+      display: 'block'
+    }}>
+        <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sobre-nos" element={<SobreNos />} />
+        <Route path="/loja" element={<Loja />} />
+      </Routes>
+      <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Semestral_work/crocs_mz/Crocs/src/component/SobreNos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SobreNos = () => {
+  return (
+    <h1>Sobre Nos</h1>
+  );
+};
+
+export default SobreNos;
