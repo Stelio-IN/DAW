8789aba416b59b6e6a5b759cdf54dea1fd132741
pedrojunior07 <132139653+pedrojunior07@users.noreply.github.com>
2024-11-08 14:18:28 +0100

    corrinho

--- Semestral_work/crocs_mz/Crocs/src/components/Navbar/index.jsx
+++ Semestral_work/crocs_mz/Crocs/src/components/Navbar/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './menu.css'; // Import your CSS file
 import LogoCrocs from '../../assets/img/crocs_logo.webp';
+import React, { useState, useEffect} from 'react' 
+import iconCart from '../../assets/images/iconCart.png' 
+import { useSelector, useDispatch } from 'react-redux' 
+import { toggleStatusTab } from '../../stores/cart'
 
 const Navbar = () => {
+
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const carts = useSelector(store => store.cart.items);
+  const dispatch = useDispatch();
+  useEffect(() => {
+      let total = 0;
+      carts.forEach(item => total += item.quantity);
+      setTotalQuantity(total);
+  }, [carts])
+  const handleOpenTabCart = () => {
+      dispatch(toggleStatusTab());
+  }
   return (
     <nav>
       <div className="wrapper">
@@ -132,6 +147,7 @@ const Navbar = () => {
           <i className='bx bx-menu'></i>
         </label>
       </div>
+      
     </nav>
   );
 };
--- Semestral_work/crocs_mz/Crocs/src/main.jsx
+++ Semestral_work/crocs_mz/Crocs/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
 import './index.css'
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; 
+import { store } from './stores';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
+
