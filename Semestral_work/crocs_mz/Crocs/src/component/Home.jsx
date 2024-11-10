645e33f8dac4f27158e573bca495a2e4bffedea3
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2024-11-10 21:21:57 +0200

    ss

--- Semestral_work/crocs_mz/Crocs/src/component/Home.jsx
+++ Semestral_work/crocs_mz/Crocs/src/component/Home.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import '../assets/style/home.css'
+import '../assets/style/home.css';
+
+
 const Home = () => {
   return (
     <div className='content'>
-      <h1>Home</h1>  
+      <main>
+        <article>
+          {/* Section 1 */}
+          <div className="Container">
+            <div className="home-container">
+              <h2>Crocs Mozambique</h2>
+              <p>
+                Novos modelos, com os últimos crocs. <br />
+                Mais confortáveis do que nunca
+              </p>
+              <button id="shop_now">Comprar</button>
+            </div>
+
+            <div className="home-container-img"> </div>
+          </div>
+
+          {/* Section 2 */}
+          <div className="Advantages-Container">
+            <div className="Discount">
+              <img src="coupon.png" alt="Desconto" />
+              <p>Descontos todas semanas</p>
+            </div>
+            <div>
+              <img src="online-support.png" alt="Suporte" />
+              <p>Suporte 24/7 dias</p>
+            </div>
+            <div>
+              <img src="delivery.png" alt="Entrega" />
+              <p>Entrega ao domicílio</p>
+            </div>
+            <div>
+              <img src="credit-card.png" alt="Pagamento seguro" />
+              <p>Pagamento seguro</p>
+            </div>
+          </div>
+     
+        </article>
+      </main>
     </div>
-    
   );
 };
 
